353b36648b3c214af4e1b39cd5b85f986e573c0a
johny1614 <johny1614@gmail.com>
2019-04-22 12:42:08 +0200

    env_3 poprawione z pomaranczowymi swiatlami

--- traffic fluid simulator/backend/env_3/runner.py
+++ traffic fluid simulator/backend/env_3/runner.py
@@ -3,9 +3,10 @@ import random
 def hash_(action):
     return tuple([tuple(a) for a in action])
 
-max_time=15
+max_time=30
 env = Env(max_time)
 for time in range(max_time-1):
-    action = hash_(random.choice(env.action_space))
-    state,reward = env.step(action)
-
+    a = random.choice(env.getActionSpace())
+    state,r,action = env.step(a)
+    print('a',action)
+    print('s',state)
